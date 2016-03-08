9454b847709eefb37411d9e25b3ee92102dc6c98
Radek Wierzbicki <radek@opsline.com>
2016-03-08 08:01:57 -0600

    adding simple, working environments tests

--- test/integration/environments/serverspec/groups_spec.rb
+++ test/integration/environments/serverspec/groups_spec.rb
@@ -4,4 +4,16 @@ set :backend, :exec
 
 describe 'groups' do
 
+  describe group('sysadmin') do
+    it { should exist }
+  end
+
+  describe group('prod') do
+    it { should exist }
+  end
+
+  describe group('qa') do
+    it { should_not exist }
+  end
+
 end
--- test/integration/environments/serverspec/users_spec.rb
+++ test/integration/environments/serverspec/users_spec.rb
@@ -4,4 +4,20 @@ set :backend, :exec
 
 describe 'users' do
 
+  describe user('user1') do
+    it { should exist }
+  end
+
+  describe user('produser') do
+    it { should exist }
+  end
+
+  describe user('qauser') do
+    it { should_not exist }
+  end
+
+  describe user('testuser') do
+    it { should_not exist }
+  end
+
 end
